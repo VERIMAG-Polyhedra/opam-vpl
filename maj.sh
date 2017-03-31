c6057b76df3fa2f7918249cfee114ebc30f7fd67
Alexandre Maréchal <alex.marechal@univ-grenoble-alpes.fr>
2017-03-31 13:13:41 +0200

    opam update

--- maj.sh
+++ maj.sh
@@ -1,1 +1,6 @@
-ssh ssh-veri.imag.fr "cd /import/www/PEOPLE/boulme/opam-vpl/ ; ./update.sh" -l $1
+make deps
+make admin
+git commit -a -m "opam update"
+git pull
+git push
+ssh ssh-veri.imag.fr "cd /import/www/PEOPLE/boulme/opam-vpl/ ; ./update.sh"
