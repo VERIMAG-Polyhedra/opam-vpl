6fa9fe1b66007e261782c707a23bfc0535d318df
Sylvain Boulmé <sylvain.boulme@univ-grenoble-alpes.fr>
2017-11-16 16:17:50 +0100

    opam update

--- maj.sh
+++ maj.sh
@@ -1,6 +1,11 @@
+if [[ "x$USER" == "xsylvain" ]]; then
+    login=boulme
+else
+    login="$USER"
+fi
 make deps
 make admin
 git commit -a -m "opam update"
 git pull
 git push
-ssh ssh-veri.imag.fr "cd /import/www/PEOPLE/boulme/opam-vpl/ ; ./update.sh"
+ssh ${login}@ssh-veri.imag.fr "cd /import/www/PEOPLE/boulme/opam-vpl/ ; ./update.sh"
