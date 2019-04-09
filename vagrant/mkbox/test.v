69b9d0f42f7fc592151645aaa7a9a596e9873459
Sylvain Boulmé <sylvain.boulme@univ-grenoble-alpes.fr>
2019-04-09 16:58:00 +0200

    update test.v for coq 8.9.0

--- vagrant/mkbox/test.v
+++ vagrant/mkbox/test.v
@@ -1,3 +1,4 @@
+Require Import BinInt.
 Require Import VplTactic.Tactic.
 Add Field Qcfield: Qcft (decidable Qc_eq_bool_correct, constants [vpl_cte]).
 
@@ -9,11 +10,12 @@ Proof.
   vpl_auto.
 Qed.
 
+
 Goal forall (v1 v2 v3: Qc) (f: Qc -> Qc),
    f ((v2 - 1)*v3) <> f ((2#3) * v1 * v2)
    -> v1+3 <= (v2 + v3)
    -> v1 <= 3*(v3-v2-1)
-   -> 2*v1 < 3*(v3-2).
+   -> 2%Z*v1 < 3*(v3-2).
 Proof.
   vpl_reduce.
   vpl_post.
